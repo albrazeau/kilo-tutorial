8db83dc47f689bd1bec2ef87ae3585368bcf4268
albrazeau <abrazeau1993@gmail.com>
2020-03-05 22:25:23 -0500

    on to the last line

--- kilo.c
+++ kilo.c
@@ -4,7 +4,7 @@ I have no idea what I am doing!
 
 https://viewsourcecode.org/snaptoken/kilo/
 
-left off at https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html#clear-the-screen
+left off at https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html#the-last-line
 */
 
 /*** includes ***/
@@ -13,6 +13,7 @@ left off at https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html#
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/ioctl.h>
 #include <termios.h>
 #include <unistd.h>
 
@@ -23,11 +24,21 @@ left off at https://viewsourcecode.org/snaptoken/kilo/03.rawInputAndOutput.html#
 /*** data ***/
 
 // maintain the original terminal structure
-struct termios orig_termios;
+struct editorConfig {
+    int screenrows;
+    int screencols;
+    struct termios orig_termios;
+};
+
+struct editorConfig E;
 
 /*** terminal ***/
 
 void die(const char *s) {
+    //clear the screen when you leave
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
     // error handling
     perror(s);
     exit(1);
@@ -35,16 +46,16 @@ void die(const char *s) {
 
 void disableRawMode() {
     // called with atexit(), returns the terminal back to normal
-    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios) == -1)
+    if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_termios) == -1)
         die("tcsetattr");
 }
 
 void enableRawMode() {  
     // turns off many option to transform the terminal into raw mode
-    if (tcgetattr(STDIN_FILENO, &orig_termios) == -1) die("tcgetattr");
+    if (tcgetattr(STDIN_FILENO, &E.orig_termios) == -1) die("tcgetattr");
     atexit(disableRawMode);
 
-    struct termios raw = orig_termios;
+    struct termios raw = E.orig_termios;
     raw.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
     raw.c_oflag &= ~(OPOST);
     raw.c_cflag |= (CS8);
@@ -65,25 +76,87 @@ char editorReadKey() {
     return c;
 }
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if (buf[i] == 'R') break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
+int getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12) return -1;
+        return getCursorPosition(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
 void editorProcessKeypress() {
     // wait for a keypress, and handle it
     char c = editorReadKey();
 
     switch (c) {
         case CTRL_KEY('q'):
-        exit(0);
-        break;
+            write(STDOUT_FILENO, "\x1b[2J", 4);
+            write(STDOUT_FILENO, "\x1b[H", 3);
+            exit(0);
+            break;
     }
 }
 
+/*** output ***/
+
+void editorDrawRows() {
+    int y;
+    for (y = 0; y < E.screenrows; y++) {
+        write(STDOUT_FILENO, "~\r\n", 3);
+    }
+}
+
+/*** input ***/
+
+void editorRefreshScreen() {
+    write(STDOUT_FILENO, "\x1b[2JK", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editorDrawRows();
+
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
 /*** init ***/
+
+void initEditor() {
+    if (getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
+}
 
 int main(){
     // enable raw mode immediately, sets disable raw at exit
     enableRawMode();
 
+    //get the number of rows and columns in the terminal
+    initEditor();
+
     // read character input with error handling, print to screen
     while(1) {
+        editorRefreshScreen();
         editorProcessKeypress();
     }
 
